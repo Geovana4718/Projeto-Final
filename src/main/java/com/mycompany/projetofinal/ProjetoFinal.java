1e4b6a4e9e981596035c149c6713cae253f9d22a
geovana4718 <geovana.augusta.sousa@gmail.com>
2025-02-19 09:08:17 -0300

    Primeiro commit do projeto

---
+++ src/main/java/com/mycompany/projetofinal/ProjetoFinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetofinal;
+
+/**
+ *
+ * @author devmat
+ */
+public class ProjetoFinal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
